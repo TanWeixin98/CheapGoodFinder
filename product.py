a9ca0cf43ef25d972ee6f2b1a877a8a98d10b5b2
weixin <weixin.tan@stonybrook.edu>
2018-09-14 21:44:56 -0400

    init the product fields

---
+++ product.py
@@ -0,0 +1,7 @@
+
+class product:
+    def __init__(self,urlLink, price, rating, locationWebsite):
+        self.url=urlLink
+        self.price= price
+        self.rating= rating
+        self.locationWebsite = locationWebsite
