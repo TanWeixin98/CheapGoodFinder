db8b3eae5a7c1e1c40fbd545ef43d79d5b1d6ffd
Justin Ying <justin1999y@gmail.com>
2018-09-15 03:54:09 -0400

    Added missing data field and commented on processes

--- product.py
+++ product.py
@@ -1,6 +1,7 @@
 import scrapy
-
+#Used to store the price, rating and url of the product searched by the user. Additionally, can be used to stoer picture urls
 class product(scrapy.item):
     price= scrapy.Field()
     url = scrapy.Field()
     rating = scrapy.Field()
+    picture_url = scrapy.Field()
