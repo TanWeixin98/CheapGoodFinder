d577edfcd0066379536e31759432108ed8cac0a2
weixin <weixin.tan@stonybrook.edu>
2018-09-14 20:08:12 -0400

    add searchterm variable/accessor method to class

---
+++ process.py
@@ -0,0 +1,16 @@
+
+#main controller for all e-commerce crawler
+
+class process:
+    def __init__(self,searchTerm):
+        searchTerm=""
+
+    def searching(self, searchTerm):
+        self.searchTerm=searchTerm
+
+    def getSearchTerm(self):
+        return self.searchTerm
+
+    
+
+
