ad098febc8ac9684dc2a2a269440705e7098aecf
weixin <weixin.tan@stonybrook.edu>
2018-09-15 02:16:19 -0400

    amazon scraper implemented; search rating,price,name,url,ima_url

--- WebScrapers/AmazonScraper.py
+++ WebScrapers/AmazonScraper.py
@@ -1,3 +1,5 @@
+import scrapy
+import csv
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
@@ -8,11 +10,31 @@ class AmazonScrapy(CrawlSpider):
         Rule(LinkExtractor(allow=(), restrict_css=('.pagnNext',)),
              callback="parse_item",
              follow=True),)
+    base_url="https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords="
     #test url
-    start_urls=["https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=shoe"]
+    start_urls=["https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=toy"]
 
     # def setBaseURL(self,URL):
     #     start_urls = URL
 
     def parse_item(self,response):
-        print("url"+response.url)
+        file_name = open('dataStorage/AmazonSearch.csv', 'w')
+        search_writer = csv.writer(file_name, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        class_selector ='.s-item-container'
+        for item in response.css(class_selector):
+            name_selector= 'a>h2 ::text'
+            price_selector= '.a-offscreen ::text'
+            rank_selector='a>i ::text'
+            url_selector='a::attr(href)'
+            img_url_selector='a>img::attr(src)'
+            yield {
+                'name': item.css(name_selector).extract_first(),
+                'price': item.css(price_selector).extract_first(),
+                'ranking': item.css(rank_selector).extract_first(),
+                'url': item.css(url_selector).extract_first(),
+                'imgURL': item.css(img_url_selector).extract_first()
+            }
+
+
+
+
